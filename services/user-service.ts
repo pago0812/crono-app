327da202887df9298c5bcb098ce7cc1c620e5743
Oscar Paredes <pago0812@gmail.com>
2022-05-14 20:29:25 -0500

    Update config

--- services/user-service.ts
+++ services/user-service.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 import {User} from '../interfaces';
 
+const NEXT_URL = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000';
+
 export const getUserService= ()=>{
-    return axios.get<User>('http://localhost:3000/api/user').then(response=>response.data);
+    return axios.get<User>(`${NEXT_URL}/api/user`).then(response=>response.data);
 }
